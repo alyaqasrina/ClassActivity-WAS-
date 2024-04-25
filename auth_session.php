3e760f8214198406ba64ed5d064a2f61d6eeab4d
alyaqasrina <92306167+alyaqasrina@users.noreply.github.com>
2024-04-25 21:56:11 +0800

    Updated auth_session.php

--- auth_session.php
+++ auth_session.php
@@ -1,7 +1,14 @@
 <?php
     session_start();
     if(!isset($_SESSION["username"])) {
-        header("Location: login.php");
+        header("Location: index.php");
+        exit();
+    }
+
+    // Check user role
+    if(!isset($_SESSION["role"])) {
+        // No role set, redirect to login page
+        header("Location: index.php");
         exit();
     }
 ?>
