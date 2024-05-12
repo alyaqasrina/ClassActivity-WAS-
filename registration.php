9e6133f09c4bf5d9b7d4bfb4d7fd395ae849fa2f
alyaqasrina <92306167+alyaqasrina@users.noreply.github.com>
2024-05-13 01:49:59 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,56 +2,52 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Registration</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
 <?php
+    header("Content-Security-Policy: default-src 'self';");
     require('db.php');
-    // When form submitted, values is inserted into the database.
-    if (isset($_REQUEST['username'])) {
-        // removes backslashes
-        $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
+    require('auth_session.php');
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    
         $username = mysqli_real_escape_string($con, $username);
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
-        
-        $query    = "INSERT into `users` (username, password, email)
-                     VALUES ('$username', '" . md5($password) . "', '$email')";
-        $result   = mysqli_query($con, $query);
-        if ($result) {
-            echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='index.php'>Login</a></p>
-                  </div>";
+        // Check if user exist in the database
+        $query    = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $user = mysqli_fetch_assoc($result);
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['role'] = $user['role'];  // Store role in session
+            // Redirect to student detail form page
+            header("Location: student_form.php");
+            exit();
         } else {
             echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='index.php'>Login</a> again.</p>
                   </div>";
         }
     } else {
 ?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <label for="role"> Select your role: 
-        <br>
-        <select name="role" class="login-input">
-            <option value="guest">Guest</option>
-            <option value="user">User</option>
-            <option value="admin">Admin</option>
-        </select></br>
-        </label>
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="index.php">Click to Login</a></p>
-    </form>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
 <?php
+    }
+?>  
+</body>
+</html>
     }
 ?>
 </body>
